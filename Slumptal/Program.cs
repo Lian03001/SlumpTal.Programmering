fe8c6f0fce76a51f778bfa4d4df64a7e197513ff
Linus <73946659+Lian03001@users.noreply.github.com>
2020-11-18 14:34:21 +0100

    Add files via upload
    Första commit, Draw.io

---
+++ Slumptal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slumptal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Tal = new Random();
+            int SlumpTal = Tal.Next(1, 101);
+            int AntalGissningar = 0;
+            Console.WriteLine("Gissa på ett tal mellan 1 och 100");
+            int Gissning = 0;
+            while (Gissning != SlumpTal)
+            {
+                Gissning = int.Parse(Console.ReadLine());
+                if (Gissning == SlumpTal)
+                {
+                    Console.WriteLine("Rätt gissat, du gissade " + AntalGissningar + " antal gångar.");
+                }
+                else
+                {
+                    AntalGissningar++;
+                    Console.WriteLine("Fel gissning, försök igen.");
+                }
+            }
+
+        }
+    }
+}
